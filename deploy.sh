66ff5905d6add4c7da65f3df6e19490fb0a3671e
Gaurav Pandey <45666264+JeeKayPee@users.noreply.github.com>
2022-03-21 10:30:12 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,5 @@
 sudo apt update
 sudo apt install docker.io --yes
-docker network create prestashop-net
-docker run -ti --name some-mysql --network prestashop-net -e MYSQL_ROOT_PASSWORD=admin -p 3307:3306 -d mysql:5.7
-docker run -ti --name some-prestashop --network prestashop-net -e DB_SERVER=some-mysql -p 8080:80 -d prestashop/prestashop:1.6
-
+sudo docker network create prestashop-net
+sudo docker run -ti --name some-mysql --network prestashop-net -e MYSQL_ROOT_PASSWORD=admin -p 3307:3306 -d mysql:5.7
+sudo docker run -ti --name some-prestashop --network prestashop-net -e DB_SERVER=some-mysql -p 8080:80 -d prestashop/prestashop:1.6
